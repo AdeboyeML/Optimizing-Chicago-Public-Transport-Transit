11d3eb5a81fd875b4789f8dbcc0b60830b616b48
AdeboyeML <oluomo_lade@yahoo.com>
2021-07-15 10:19:52 -0400

    add kafka tutorial

---
+++ kafka_tutorials/exercise6.1.py
@@ -0,0 +1,29 @@
+# Please complete the TODO items in this code
+
+import faust
+
+#
+# TODO: Create the faust app with a name and broker
+#       See: https://faust.readthedocs.io/en/latest/userguide/application.html#application-parameters
+#
+app = faust.App('first-stream-App', broker='localhost:9092')
+
+#
+# TODO: Connect Faust to com.udacity.streams.clickevents
+#       See: https://faust.readthedocs.io/en/latest/userguide/application.html#app-topic-create-a-topic-description
+#
+topic = app.topic('com.udacity.streams.clickevents') #kafka-topics --list --zookeeper localhost:2181 
+
+#
+# TODO: Provide an app agent to execute this function on topic event retrieval
+#       See: https://faust.readthedocs.io/en/latest/userguide/application.html#app-agent-define-a-new-stream-processor
+#
+@app.agent(topic)
+async def clickevent(clickevents):
+    # TODO: Define the async for loop that iterates over clickevents
+    #       See: https://faust.readthedocs.io/en/latest/userguide/agents.html#the-stream
+    async for clickevent in clickevents:
+        print(clickevent)
+
+if __name__ == "__main__":
+    app.main()
